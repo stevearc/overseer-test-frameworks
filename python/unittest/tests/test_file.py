47906dd7a61a56a257fbeede093eb39d89827f28
Steven Arcangeli <stevearc@stevearc.com>
2022-06-05 15:48:42 -0700

    Add some sleeps to python unittest sample

--- python/unittest/tests/test_file.py
+++ python/unittest/tests/test_file.py
@@ -1,21 +1,26 @@
 import sys
+import time
 import unittest
 
 
 class TestGroup(unittest.TestCase):
     def test_succeed(self):
-        pass
+        time.sleep(0.2)
 
     def test_skip(self):
+        time.sleep(0.2)
         self.skipTest("Skip this test")
 
     def test_fail(self):
+        time.sleep(0.2)
         self.assertTrue(False)
 
     def test_error(self):
+        time.sleep(0.2)
         self.foo.bar
 
     def test_fail_with_output(self):
         print("This is some output")
         sys.stderr.write("This is some stderr output\n")
+        time.sleep(0.2)
         self.assertTrue(False)
